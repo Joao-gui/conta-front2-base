217eff7bd9f83edc03749618f11591489b7816a2
Joao <joao.pellacani@db1.com.br>
2019-08-29 21:29:36 -0300

    Commit Front Funcionando

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app> 
+    <div>
+      <v-toolbar dark>
+        <v-toolbar-title>Conta</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <router-link to="/">
+            <v-btn class="link" text>Home</v-btn>
+          </router-link>
+          <router-link to="/cidades">
+            <v-btn class="link" text>Cidades</v-btn>
+          </router-link>
+          <router-link to="/agencias">
+            <v-btn class="link" text>Agências</v-btn>
+          </router-link>
+          <router-link to="/clientes">
+            <v-btn class="link" text>Clientes</v-btn>
+          </router-link>
+        </v-toolbar-items>        
+      </v-toolbar>
+    </div>
+    <router-view></router-view>  
+  </v-app>
+</template>
+<style scoped>
+.link{
+  margin-top: 10px;
+}
+a{
+  text-decoration: none;
+}
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Bem vindo ao sistema de contas do banco DB1 Start</h1>
+</template>
